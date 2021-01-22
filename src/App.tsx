b41b2fbefb248e7646f0941ef48be1142305e301
sheng <1252822413@qq.com>
2021-01-22 11:28:17 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Author: sheng
+ * @Date: 2021-01-22 11:20:45
+ * @LastEditTime: 2021-01-22 11:24:14
+ * @FilePath: /reactlearn/src/App.tsx
+ */
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className='App'>奥术大师大</div>
 }
 
-export default App;
+export default App
